1cf2f751621712527028c41d2ddec3c90410539c
T-R Creation <momox493@gmail.com>
2025-07-25 22:38:38 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,16 @@
 Config = {}
-Config.Locale = "en" -- 语言
+Config.Locale = "zh-cn" -- 语言
 Config.NotificationPosition = "top-right" -- 支持: top-left, top-center, top-right, bottom-left, bottom-right, bottom-center
+-- Config.DebugMode = true -- Test mode: true = show console notifications for player connect/disconnect/join/die, false = don't show
+
+--  Configuration  for  key  bindings
+      Config.XKey  =  73  --  Default  X  key
+      Config.ZKey  =  20  --  Default  Z  key
+      Config.GKey  =  47 --  Default  G  key
+      Config.NKey  =  249    --  Default  N  key
+
+-- ⭐ UI 配置可以前往 UI.html的 74行至81行 进行UI更改配置。⭐
+-- ⭐ For UI configuration, go to lines 74 to 81 of UI.html to change the UI configuration. ⭐
 
 -- 中文简体（zh-cn）
 
