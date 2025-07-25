e71e5339aa1a13aa2955ceb5b136aa382261679d
T-R Creation <momox493@gmail.com>
2025-07-25 22:39:06 +0800

    Update lang.lua

--- lang.lua
+++ lang.lua
@@ -4,75 +4,97 @@ Lang["zh-cn"] = {
     handsup = "~INPUT_VEH_DUCK~ 放下双手\n~INPUT_HUD_SPECIAL~ 跪下\n~INPUT_DETONATE~  趴下\n~INPUT_PUSH_TO_TALK~ 返回到举手",
     kneel = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_HUD_SPECIAL~ 双手抱头\n~INPUT_PUSH_TO_TALK~ 返回到举手",
     prone = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_PUSH_TO_TALK~ 返回到举手",
-    kneel_handsup = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_PUSH_TO_TALK~ 返回到举手\n~INPUT_HUD_SPECIAL~ 回到跪下"
+    kneel_handsup = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_PUSH_TO_TALK~ 返回到举手\n~INPUT_HUD_SPECIAL~ 回到跪下",
+    interrupted = "动作被中断",
+    weapon_interrupted = "使用武器中断动作"
 }
 
 Lang["zh-tw"] = {
     handsup = "~INPUT_VEH_DUCK~ 放下雙手\n~INPUT_HUD_SPECIAL~ 跪下\n~INPUT_DETONATE~ 趴下\n~INPUT_PUSH_TO_TALK~ 返回到舉手",
     kneel = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_HUD_SPECIAL~ 雙手抱頭\n~INPUT_PUSH_TO_TALK~ 返回到舉手",
     prone = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_PUSH_TO_TALK~ 返回到舉手",
-    kneel_handsup = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_PUSH_TO_TALK~ 返回到舉手\n~INPUT_HUD_SPECIAL~ 回到跪下"
+    kneel_handsup = "~INPUT_VEH_DUCK~ 起身逃跑\n~INPUT_PUSH_TO_TALK~ 返回到舉手\n~INPUT_HUD_SPECIAL~ 回到跪下",
+    interrupted = "動作被中斷",
+    weapon_interrupted = "使用武器中斷動作"
 }
 
 Lang["zh-hk"] = {
     handsup = "~INPUT_VEH_DUCK~ 放低雙手\n~INPUT_HUD_SPECIAL~ 跪低\n~INPUT_DETONATE~ 趴低\n~INPUT_PUSH_TO_TALK~ 返回舉手",
     kneel = "~INPUT_VEH_DUCK~ 起身走人\n~INPUT_HUD_SPECIAL~ 雙手抱頭\n~INPUT_PUSH_TO_TALK~ 返回舉手",
     prone = "~INPUT_VEH_DUCK~ 起身走人\n~INPUT_PUSH_TO_TALK~ 返回舉手",
-    kneel_handsup = "~INPUT_VEH_DUCK~ 起身走人\n~INPUT_PUSH_TO_TALK~ 返回舉手\n~INPUT_HUD_SPECIAL~ 返回跪低"
+    kneel_handsup = "~INPUT_VEH_DUCK~ 起身走人\n~INPUT_PUSH_TO_TALK~ 返回舉手\n~INPUT_HUD_SPECIAL~ 返回跪低",
+    interrupted = "動作被中斷",
+    weapon_interrupted = "使用武器中斷動作"
 }
 
 Lang["en"] = {
     handsup = "~INPUT_VEH_DUCK~ Put your hands down\n~INPUT_HUD_SPECIAL~ Kneel\n~INPUT_DETONATE~ Go prone",
     kneel = "~INPUT_VEH_DUCK~ Stand up and run\n~INPUT_HUD_SPECIAL~ Hands on head\n~INPUT_PUSH_TO_TALK~ Back to hands up",
     prone = "~INPUT_VEH_DUCK~ Stand up and run\n~INPUT_PUSH_TO_TALK~ Back to hands up",
-    kneel_handsup = "~INPUT_VEH_DUCK~ Stand up and run\n~INPUT_PUSH_TO_TALK~ Back to hands up\n~INPUT_HUD_SPECIAL~ Back to kneel"
+    kneel_handsup = "~INPUT_VEH_DUCK~ Stand up and run\n~INPUT_PUSH_TO_TALK~ Back to hands up\n~INPUT_HUD_SPECIAL~ Back to kneel",
+    interrupted = "Action interrupted",
+    weapon_interrupted = "Weapon use interrupted action"
 }
 
 Lang["es"] = {
     handsup = "~INPUT_VEH_DUCK~ Bajar las manos\n~INPUT_HUD_SPECIAL~ Arrodillarse\n~INPUT_DETONATE~ Tumbarse",
     kneel = "~INPUT_VEH_DUCK~ Levantarse y huir\n~INPUT_HUD_SPECIAL~ Manos en la cabeza\n~INPUT_PUSH_TO_TALK~ Volver a manos arriba",
     prone = "~INPUT_VEH_DUCK~ Levantarse y huir\n~INPUT_PUSH_TO_TALK~ Volver a manos arriba",
-    kneel_handsup = "~INPUT_VEH_DUCK~ Levantarse y huir\n~INPUT_PUSH_TO_TALK~ Volver a manos arriba\n~INPUT_HUD_SPECIAL~ Volver a arrodillarse"
+    kneel_handsup = "~INPUT_VEH_DUCK~ Levantarse y huir\n~INPUT_PUSH_TO_TALK~ Volver a manos arriba\n~INPUT_HUD_SPECIAL~ Volver a arrodillarse",
+    interrupted = "Acción interrumpida",
+    weapon_interrupted = "Uso de arma interrumpió la acción"
 }
 
 Lang["ko"] = {
     handsup = "~INPUT_VEH_DUCK~ 손 내리기\n~INPUT_HUD_SPECIAL~ 무릎 꿇기\n~INPUT_DETONATE~ 엎드리기",
     kneel = "~INPUT_VEH_DUCK~ 일어나서 도망가기\n~INPUT_HUD_SPECIAL~ 머리 위로 손 올리기\n~INPUT_PUSH_TO_TALK~ 손 들기로 돌아가기",
     prone = "~INPUT_VEH_DUCK~ 일어나서 도망가기\n~INPUT_PUSH_TO_TALK~ 손 들기로 돌아가기",
-    kneel_handsup = "~INPUT_VEH_DUCK~ 일어나서 도망가기\n~INPUT_PUSH_TO_TALK~ 손 들기로 돌아가기\n~INPUT_HUD_SPECIAL~ 무릎 꿇기로 돌아가기"
+    kneel_handsup = "~INPUT_VEH_DUCK~ 일어나서 도망가기\n~INPUT_PUSH_TO_TALK~ 손 들기로 돌아가기\n~INPUT_HUD_SPECIAL~ 무릎 꿇기로 돌아가기",
+    interrupted = "동작이 중단됨",
+    weapon_interrupted = "무기 사용으로 동작 중단"
 }
 
 Lang["fr"] = {
     handsup = "~INPUT_VEH_DUCK~ Baisser les mains\n~INPUT_HUD_SPECIAL~ S'agenouiller\n~INPUT_DETONATE~ Se coucher\n~INPUT_PUSH_TO_TALK~ Retourner à mains levées",
     kneel = "~INPUT_VEH_DUCK~ Se relever et fuir\n~INPUT_HUD_SPECIAL~ Mains sur la tête\n~INPUT_PUSH_TO_TALK~ Retourner à mains levées",
     prone = "~INPUT_VEH_DUCK~ Se relever et fuir\n~INPUT_PUSH_TO_TALK~ Retourner à mains levées",
-    kneel_handsup = "~INPUT_VEH_DUCK~ Se relever et fuir\n~INPUT_PUSH_TO_TALK~ Retourner à mains levées\n~INPUT_HUD_SPECIAL~ Retourner à s'agenouiller"
+    kneel_handsup = "~INPUT_VEH_DUCK~ Se relever et fuir\n~INPUT_PUSH_TO_TALK~ Retourner à mains levées\n~INPUT_HUD_SPECIAL~ Retourner à s'agenouiller",
+    interrupted = "Action interrompue",
+    weapon_interrupted = "Utilisation d'arme a interrompu l'action"
 }
 
 Lang["de"] = {
     handsup = "~INPUT_VEH_DUCK~ Hände runter\n~INPUT_HUD_SPECIAL~ Knien\n~INPUT_DETONATE~ Hinlegen\n~INPUT_PUSH_TO_TALK~ Zurück zu Hände hoch",
     kneel = "~INPUT_VEH_DUCK~ Aufstehen und fliehen\n~INPUT_HUD_SPECIAL~ Hände auf den Kopf\n~INPUT_PUSH_TO_TALK~ Zurück zu Hände hoch",
     prone = "~INPUT_VEH_DUCK~ Aufstehen und fliehen\n~INPUT_PUSH_TO_TALK~ Zurück zu Hände hoch",
-    kneel_handsup = "~INPUT_VEH_DUCK~ Aufstehen und fliehen\n~INPUT_PUSH_TO_TALK~ Zurück zu Hände hoch\n~INPUT_HUD_SPECIAL~ Zurück zu knien"
+    kneel_handsup = "~INPUT_VEH_DUCK~ Aufstehen und fliehen\n~INPUT_PUSH_TO_TALK~ Zurück zu Hände hoch\n~INPUT_HUD_SPECIAL~ Zurück zu knien",
+    interrupted = "Aktion unterbrochen",
+    weapon_interrupted = "Waffennutzung unterbrach Aktion"
 }
 
 Lang["pt"] = {
     handsup = "~INPUT_VEH_DUCK~ Baixar as mãos\n~INPUT_HUD_SPECIAL~ Ajoelhar\n~INPUT_DETONATE~ Deitar\n~INPUT_PUSH_TO_TALK~ Voltar para mãos ao alto",
     kneel = "~INPUT_VEH_DUCK~ Levantar e fugir\n~INPUT_HUD_SPECIAL~ Mãos na cabeça\n~INPUT_PUSH_TO_TALK~ Voltar para mãos ao alto",
     prone = "~INPUT_VEH_DUCK~ Levantar e fugir\n~INPUT_PUSH_TO_TALK~ Voltar para mãos ao alto",
-    kneel_handsup = "~INPUT_VEH_DUCK~ Levantar e fugir\n~INPUT_PUSH_TO_TALK~ Voltar para mãos ao alto\n~INPUT_HUD_SPECIAL~ Voltar para ajoelhar"
+    kneel_handsup = "~INPUT_VEH_DUCK~ Levantar e fugir\n~INPUT_PUSH_TO_TALK~ Voltar para mãos ao alto\n~INPUT_HUD_SPECIAL~ Voltar para ajoelhar",
+    interrupted = "Ação interrompida",
+    weapon_interrupted = "Uso de arma interrompeu ação"
 }
 
 Lang["ja"] = {
     handsup = "~INPUT_VEH_DUCK~ 手を下ろす\n~INPUT_HUD_SPECIAL~ ひざまずく\n~INPUT_DETONATE~ 伏せる\n~INPUT_PUSH_TO_TALK~ 手を挙げるに戻る",
     kneel = "~INPUT_VEH_DUCK~ 立ち上がって逃げる\n~INPUT_HUD_SPECIAL~ 頭の上に手\n~INPUT_PUSH_TO_TALK~ 手を挙げるに戻る",
     prone = "~INPUT_VEH_DUCK~ 立ち上がって逃げる\n~INPUT_PUSH_TO_TALK~ 手を挙げるに戻る",
-    kneel_handsup = "~INPUT_VEH_DUCK~ 立ち上がって逃げる\n~INPUT_PUSH_TO_TALK~ 手を挙げるに戻る\n~INPUT_HUD_SPECIAL~ ひざまずくに戻る"
+    kneel_handsup = "~INPUT_VEH_DUCK~ 立ち上がって逃げる\n~INPUT_PUSH_TO_TALK~ 手を挙げるに戻る\n~INPUT_HUD_SPECIAL~ ひざまずくに戻る",
+    interrupted = "アクションが中断されました",
+    weapon_interrupted = "武器の使用で動作が中断されました"
 }
 
 Lang["ru"] = {
     handsup = "~INPUT_VEH_DUCK~ Опустить руки\n~INPUT_HUD_SPECIAL~ Встать на колени\n~INPUT_DETONATE~ Лечь\n~INPUT_PUSH_TO_TALK~ Вернуться к поднятым рукам",
     kneel = "~INPUT_VEH_DUCK~ Встать и убежать\n~INPUT_HUD_SPECIAL~ Руки на голову\n~INPUT_PUSH_TO_TALK~ Вернуться к поднятым рукам",
     prone = "~INPUT_VEH_DUCK~ Встать и убежать\n~INPUT_PUSH_TO_TALK~ Вернуться к поднятым рукам",
-    kneel_handsup = "~INPUT_VEH_DUCK~ Встать и убежать\n~INPUT_PUSH_TO_TALK~ Вернуться к поднятым рукам\n~INPUT_HUD_SPECIAL~ Вернуться к коленям"
+    kneel_handsup = "~INPUT_VEH_DUCK~ Встать и убежать\n~INPUT_PUSH_TO_TALK~ Вернуться к поднятым рукам\n~INPUT_HUD_SPECIAL~ Вернуться к коленям",
+    interrupted = "Действие прервано",
+    weapon_interrupted = "Действие прервано из-за использования оружия"
 } 
